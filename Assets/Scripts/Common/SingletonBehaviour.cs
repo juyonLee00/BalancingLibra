c353498a6876853f00e661deb13bc5804475af41
juyonLee00 <ljy012900@gmail.com>
2026-01-03 23:44:29 +0900

    [Core] feat : 전역 매니저 관리를 위한 SingletonBehaviour<T> 제네릭 클래스 구현 (#4)
    1. MonoBehaviour 상속받는 매니저 클래스들이 재사용 가능한 제네릭 기반
       싱글톤 클래스 구현
    
    2. 라이프사이클 관리
       - Lazy Initialization : 호출 시점 객체가 없으면 자동 생성
       - Ghost Object 예방 : 애플리케이션 종료 과정에서 파괴된 싱글톤 재생성
         차단
       - Thread Safety : lock을 통해 멀티 스레드 환경에서 race condition
         방지
    
    Refs #4

---
+++ Assets/Scripts/Common/SingletonBehaviour.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+
+public class SingletonBehaviour<T> : MonoBehaviour where T : SingletonBehaviour<T>
+{
+    private static object _lock = new object();
+    private static bool _applicationIsQuitting = false;
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            //게임 종료 시 null 반환
+            if(_applicationIsQuitting)
+            {
+                Logger.LogError($"[SingletonBehaviour] Instance '{typeof(T)}' is already destroyed. Returning null.");
+                return null;
+            }
+
+            //스레드 안전성 확보
+            lock(_lock)
+            {
+                if(_instance == null)
+                {
+                    //씬에 이미 존재하는지 찾기
+                    _instance = FindAnyObjectByType<T>();
+
+                    if(FindObjectsByType<T>(FindObjectsSortMode.None).Length > 1)
+                    {
+                        Logger.LogError($"[SingletonBehaviour] Multiple instances of '{typeof(T)}' found. Reopening the scene might fix it.");
+                        return _instance;
+                    }
+                    
+                    //씬에 없으면 새로 생성
+                    else if(_instance == null)
+                    {
+                        GameObject singleton = new GameObject();
+                        _instance = singleton.AddComponent<T>();
+                        singleton.name = "[Singleton] {typeof(T)}";
+
+                        DontDestroyOnLoad(singleton);
+
+                        Logger.Log($"[SingletonBehaviour] An instance of '{typeof(T)}' is needed in the scene, so '{singleton}' was created with DontDestroyOnLoad.");
+                    }
+
+                    //씬에 없으면 새로 생성
+                    else
+                    {
+                        DontDestroyOnLoad(_instance.gameObject);
+                        Logger.Log($"[SingletonBehaviour] An instance of '{typeof(T)}' is needed in the scene, so '{_instance.gameObject}' was created with DontDestroyOnLoad.");
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        Init();
+    }
+
+    //중복 생성 방지
+    protected virtual void Init()
+    {
+        if(_instance == null)
+        {
+            _instance = (T)this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //삭제 시 실행
+    protected virtual void OnDestroy()
+    {
+        Dispose();
+    }
+
+    protected virtual void Dispose()
+    {
+        if (_instance == this)
+        {
+            _applicationIsQuitting = true;
+        }
+    }
+}
