3256554009404bbee1b7caac003b29e81d1941ab
juyonLee00 <ljy012900@gmail.com>
2026-01-25 20:40:11 +0900

    chore : DataManager 내부 주석 수정

--- Assets/Scripts/DataTableManager.cs
+++ Assets/Scripts/DataTableManager.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections.Generic;
 using BalancingLibra.Data;
-using UnityEngine;
-
 public class DataTableManager : SingletonBehaviour<DataTableManager>
 {
-    //모든 테이블 타입 key-value로 설정해서 보관
+    // 모든 테이블 타입 key-value로 설정해서 보관
     private Dictionary<Type, IDataTable> _tables = new Dictionary<Type, IDataTable>();
 
     protected override void Init()
@@ -16,13 +14,13 @@ public class DataTableManager : SingletonBehaviour<DataTableManager>
 
     public void LoadAllTables()
     {
-        //게임에 필요한 모든 데이터 테이블 등록 및 로드
-        //LoadTable<ItemTable>("Data/ItemTable");
+        // 게임에 필요한 모든 데이터 테이블 등록 및 로드
+        // LoadTable<ItemTable>("Data/ItemTable");
 
         Logger.Log("DataTableManager : All tables loaded");
     }
 
-    //제네릭 로드 함수: 테이블 객체 생성 후 Load 호출, 딕셔너리에 등록
+    // 제네릭 로드 함수: 테이블 객체 생성 후 Load 호출, 딕셔너리에 등록
     private void LoadTable<T>(string path) where T : IDataTable, new()
     {
         T table = new T();
@@ -34,7 +32,7 @@ public class DataTableManager : SingletonBehaviour<DataTableManager>
             _tables.Add(typeof(T), table);
     }
 
-    //외부에서 특정 테이블 가져오는 함수
+    // 외부에서 특정 테이블 가져오는 함수
     public T Get<T>() where T : class, IDataTable
     {
         if(_tables.TryGetValue(typeof(T), out IDataTable table))
